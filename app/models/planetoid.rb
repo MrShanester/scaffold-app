be57715e3a5259ab9b94ce0063f5dbb7bb2e3a4c
Shane Nicholson <shanescottnicholson@gmail.com>
2021-11-05 09:40:42 -0700

    Adds validations for planetoids

--- app/models/planetoid.rb
+++ app/models/planetoid.rb
@@ -1,2 +1,5 @@
 class Planetoid < ApplicationRecord
+  attr_accessible :name, :mass, :diameter, :orbital_distance
+  validates :name, :mass, :diameter, :orbital_distance, :presence = true
+  validates :mass, :diameter, :orbital_distance, numericality: { only_integer: true,  }, :greater_than {10000}
 end
